8ab5070c5e63989edf4f9cc434607d5af7104730
Ilham Izzul Hadyan <ilhamizzul@gmail.com>
2021-11-10 09:57:29 +0700

    adding environment variable

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
 const Hapi = require('hapi');
 const routes = require('./routes');
+const config = require('../config');
 
 const init = async () => {
     const server = Hapi.server({
-        port: 5000,
-        host: 'localhost',
+        port: config.PORT,
+        host: config.HOST,
     });
     server.route(routes);
 
     await server.start();
+    console.log(`NODE_ENV=${config.NODE_ENV}`);
     console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
